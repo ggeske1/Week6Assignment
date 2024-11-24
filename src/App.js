baa53741475739d77b656f2950a7a92b37de6f90
Grayden <graydengeske@gmail.com>
2024-11-23 22:10:15 -0600

    Working temp converter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  const [temperature, setTemperature] = useState('');
+  const [convertedTemp, setConvertedTemp] = useState('');
+
+  const handleInputChange = (event) =>{
+    setTemperature(event.target.value);
+  }
+
+  const convertToFahrenheit = () => {
+    const fahrenheit = (temperature * (9/5)) +32;
+    setConvertedTemp(fahrenheit + " degrees Fahrenheit");
+  };
+  const convertToCelsius = () => {
+    const celsius = (temperature - 32)*(5/9);
+    setConvertedTemp(celsius + " degrees Celsius");
+  };
+
+  return(
+    <div style= {{textAlign: 'center', marginTop: '50px'}}>
+      <h1>Temperature Converter</h1>
+      <input type="number" value={temperature} onChange={handleInputChange}/>
+      <button onClick={convertToFahrenheit} style={{margin: '5px'}}>Convert To Fahrenheit</button>
+      <button onClick={convertToCelsius} style={{margin: '5px'}}>Convert To Celsius</button>
+      <h2>Result: {convertedTemp}</h2>
     </div>
   );
-}
 
+}
 export default App;
